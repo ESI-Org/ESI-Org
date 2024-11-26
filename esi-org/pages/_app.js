5961eb7f9a2f70cdd196d42ce6a7a8c3a6e76656
Nico Liao <40799837+nico-liao0918@users.noreply.github.com>
2024-11-26 20:26:38 +0800

    Update _app.js

--- esi-org/pages/_app.js
+++ esi-org/pages/_app.js
@@ -1,5 +1,42 @@
 import "@/styles/globals.css";
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+ getDefaultConfig,
+ RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+ mainnet,
+ polygon,
+ holesky,
+ optimism,
+ arbitrum,
+ base,
+} from 'wagmi/chains';
+import {
+ QueryClientProvider,
+ QueryClient,
+} from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+ appName: 'My RainbowKit App',
+ projectId: 'YOUR_PROJECT_ID',
+ chains: [mainnet, polygon, holesky, optimism, arbitrum, base],
+ ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+ const queryClient = new QueryClient();
+ return (
+ <WagmiProvider config={config}>
+ <QueryClientProvider client={queryClient}>
+ <RainbowKitProvider>
+ <Component {...pageProps} />
+ </RainbowKitProvider>
+ </QueryClientProvider>
+ </WagmiProvider>
+ );
 }
